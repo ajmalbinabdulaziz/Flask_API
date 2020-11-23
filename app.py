f100cf9cb45f99131553e94e4d135b4f658a2d08
ajmalbinabdulaziz <ajmalbinabdulaziz@gmail.com>
2020-11-23 12:42:37 +0530

    Improving Code and Error Control

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+items = []
+
+class Item(Resource):
+    def get(self, name):
+        item = next(filter(lambda a: a['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404   
+
+    def post(self, name):
+        if next(filter(lambda a: a['name'] == name, items), None):
+            return f"An item with name {name} already exists", 400
+        
+
+
+        data = request.get_json()
+        item =  { 'name': name, 'price': data['price'] }
+        items.append(item)   
+        return item, 201
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+api.add_resource(Item, '/item/<string:name>')  
+api.add_resource(ItemList, '/items')    
+
+app.run(port=5000, debug=True) 
