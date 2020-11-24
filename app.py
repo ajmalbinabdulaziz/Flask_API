e6b0e2f70347dc394f30c9311c9ef739fc206ac9
ajmalbinabdulaziz <ajmalbinabdulaziz@gmail.com>
2020-11-24 20:36:10 +0530

    Authentication and Logging

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'ajmal'
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
 
 items = []
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda a: a['name'] == name, items), None)
         return {'item': item}, 200 if item else 404   
 
     def post(self, name):
         if next(filter(lambda a: a['name'] == name, items), None):
-            return f"An item with name {name} already exists", 400
-        
+            return f"An item with name {name} already exists", 400       
 
 
         data = request.get_json()
