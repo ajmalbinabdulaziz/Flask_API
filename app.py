f2092a29adb3817ee12281bcaf569219f32deafe
ajmalbinabdulaziz <ajmalbinabdulaziz@gmail.com>
2020-11-25 17:29:22 +0530

    Optimising the final code and request parsing

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
 
@@ -12,6 +12,10 @@ jwt = JWT(app, authenticate, identity)
 items = []
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument("price",type=float, required=True, help="This field can't be left blank")
+
+
     @jwt_required()
     def get(self, name):
         item = next(filter(lambda a: a['name'] == name, items), None)
@@ -22,7 +26,8 @@ class Item(Resource):
             return f"An item with name {name} already exists", 400       
 
 
-        data = request.get_json()
+        
+        data = Item.parser.parse_args()
         item =  { 'name': name, 'price': data['price'] }
         items.append(item)   
         return item, 201
@@ -32,8 +37,8 @@ class Item(Resource):
         items = list(filter(lambda x : x['name'] != name, items))
         return { "message": "item deleted" }
 
-    def put(self, name):
-        data = request.get_json()
+    def put(self, name):       
+        data = Item.parser.parse_args()
         item = next(filter(lambda a : a['name'] == name, items), None)
         if item is None:
             item =  { 'name': name, 'price': data['price'] }
