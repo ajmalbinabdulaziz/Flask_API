c023ac271036e8d51e0fa6286f5d8a29c702da69
ajmalbinabdulaziz <ajmalbinabdulaziz@gmail.com>
2020-12-01 20:55:54 +0530

    Writing our Item resources to a database

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
-from flask_jwt import JWT, jwt_required
+from flask_jwt import JWT
+
 from security import authenticate, identity
 from user import UserRegister
+from item import Item, ItemList
 
 app = Flask(__name__)
 app.secret_key = 'ajmal'
@@ -10,50 +12,6 @@ api = Api(app)
 
 jwt = JWT(app, authenticate, identity)
 
-items = []
-
-class Item(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument("price",type=float, required=True, help="This field can't be left blank")
-
-
-    @jwt_required()
-    def get(self, name):
-        item = next(filter(lambda a: a['name'] == name, items), None)
-        return {'item': item}, 200 if item else 404   
-
-    def post(self, name):
-        if next(filter(lambda a: a['name'] == name, items), None):
-            return f"An item with name {name} already exists", 400       
-
-
-        
-        data = Item.parser.parse_args()
-        item =  { 'name': name, 'price': data['price'] }
-        items.append(item)   
-        return item, 201
-
-    def delete(self, name):
-        global items
-        items = list(filter(lambda x : x['name'] != name, items))
-        return { "message": "item deleted" }
-
-    def put(self, name):       
-        data = Item.parser.parse_args()
-        item = next(filter(lambda a : a['name'] == name, items), None)
-        if item is None:
-            item =  { 'name': name, 'price': data['price'] }
-            items.append(item)    
-        else:
-            item.update(data)
-
-        return item
-
-
-
-class ItemList(Resource):
-    def get(self):
-        return {'items': items}
 
 api.add_resource(Item, '/item/<string:name>')  
 api.add_resource(ItemList, '/items')    
--- user.py
+++ user.py
@@ -45,11 +45,14 @@ class UserRegister(Resource):
     parser.add_argument("username", type=str, required=True, help="This field can't be left blank" )
     parser.add_argument("password", type=str, required=True, help="This field can't be left blank" )
 
-    def post(self):
+    def post(self):       
+        data = UserRegister.parser.parse_args()
+
+        if User.find_by_username(data['username']):
+            return { "message": "A user with that username already exists" }, 400
+
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
-
-        data = UserRegister.parser.parse_args()
 
         query = "INSERT INTO users VALUES (NULL, ?, ?)"
         cursor.execute(query, (data['username'], data['password']))
